53d64855247e3f799e06c381fb2149c4b424c26a
johnsonclound <66070242@kmitl.ac.th>
2025-07-23 21:38:59 +0700

    create api and data based that connect to docker

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+require('dotenv').config();
+const mysql = require('mysql2');
 const app = express();
 
 const db = mysql.createConnection({
@@ -15,14 +17,45 @@ app.get('/', (req, res)=>{
 
 app.get('/products', (req, res) => {
     /* fetch all goods data and return JSON*/
+    const sql = 'SELECT * FROM products';
+    db.query(sql, (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Database query failed' });
+        }else {
+            res.status(200).json(results);
+        }
+        
+    });
 })
 
 app.get('/products/:id', (req, res) => {
     /* fetch goods data based on id */
-})
+    const id = Number(req.params.id);
+    const sql = 'SELECT * FROM products WHERE id = ?';
+    db.query(sql, [id], (err, results) => {
+        if(err) {
+            res.status(500).json({ message: 'Error occurred while retrieveing product.', error: err});
+        } else{
+            if (results.length === 0) {
+                res.status(404).json({ message: 'Product not found.' });
+            } else {
+                res.status(200).json({ message: 'Product retrieved successfully.', data: results });
+            }
+        }
+    });
+});
 
 app.get('/products/search/:keyword', (req, res) => {
     /* find goods with keyword(find it from name) */
+    const keyword = req.params.keyword;
+    const sql = 'SELECT * FROM products WHERE name LIKE ?';
+    db.query(sql, [`%${keyword}%`], (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Error occurred while retrieving products.', error: err });
+        } else {
+            res.status(200).json(results);
+        }
+    });
 })
 app.listen(3000, () => console.log(' Server running on port 3000'))
 
